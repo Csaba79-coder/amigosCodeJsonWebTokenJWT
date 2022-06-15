52a8f0facc72384a242a48ed2917652d06fa9902
Csaba79-coder <csabavadasz79@gmail.com>
2022-06-15 16:39:11 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/amigoscodejsonwebtokenjwt/AmigosCodeJsonWebTokenJwtApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.amigoscodejsonwebtokenjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodeJsonWebTokenJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosCodeJsonWebTokenJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/amigoscodejsonwebtokenjwt/AmigosCodeJsonWebTokenJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.amigoscodejsonwebtokenjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosCodeJsonWebTokenJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
